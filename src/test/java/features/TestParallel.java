5e5a0268fda300525e2377b7f059ddde0062bd5e
JyotiProkash <cse0718@gmail.com>
2023-04-26 23:26:27 +0600

    implemented parallel execution test class.

--- src/test/java/features/TestParallel.java
+++ src/test/java/features/TestParallel.java
@@ -1,54 +1,48 @@
 package features;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import net.masterthought.cucumber.presentation.PresentationMode;
+import net.masterthought.cucumber.sorting.SortingMethod;
 import org.apache.commons.io.FileUtils;
-import org.junit.Test;
 
-import com.intuit.karate.KarateOptions;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import com.intuit.karate.junit5.Karate.Test;
+
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 
-@KarateOptions(features = {
-	    "classpath:features/AllEmployees.feature",
-	    "classpath:features/DeleteRecord.feature",
-	    "classpath:features/CreateAnEmployee.feature",
-	    "classpath:features/SpecificEmployee.feature",
-	    "classpath:features/UpdateEmployeeRecord.feature"})
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestParallel 
+public class TestParallel
    {
-	
-	@Test
-    public void testParallel() {
-		System.setProperty("karate.env", "dev");
-        //String karateOutputPath="target/surefire-reports";
-        Results results = Runner.parallel(getClass(), 1);
-        generateReport(results.getReportDir());
-        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
-    } 
-	
-	 private static void generateReport(String karateOutputPath) 
-	    {
-		 
-		    Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
-	        List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
-	        jsonFiles.forEach(file->jsonPaths.add(file.getAbsolutePath()));
-	        /*for (File file : jsonFiles) {
-	            jsonPaths.add(file.getAbsolutePath());
-	        }*/
-	        
-		    Configuration config = new Configuration(new File("target"), "KarateAPITest");
-	        config.addClassifications("dev", System.getProperty("karate.env"));
-	        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
-	        reportBuilder.generateReports();
-	    }
+	   @Test
+	   public void testParallel() {
+		   //System.setProperty("karate.env", "dev");
+		   Results results = Runner
+				   .path("classpath:features")
+				   .outputCucumberJson(true)
+				   //.tags("@abc", "~@ignore")
+				   .parallel(1);
+		   generateReport(results.getReportDir());
+		   assertEquals(0, results.getFailCount(), results.getErrorMessages());
+	   }
+
+	   public static void generateReport(String karateOutputPath) {
+		   Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+		   List<String> jsonPaths = new ArrayList(jsonFiles.size());
+		   jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+		   Configuration config = new Configuration(new File("target"), "APIAutomation");
+		   config.setSortingMethod(SortingMethod.NATURAL);
+		   // optional configuration - check javadoc for details
+		   config.addPresentationModes(PresentationMode.EXPAND_ALL_STEPS);
+		   config.setTrendsStatsFile(new File("target/demo-trends.json"));
+		   ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+		   reportBuilder.generateReports();
+	   }
 	 
    }
